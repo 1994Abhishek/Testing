489e150bff1394da06041d2e3efc5a8fc47858f7
1994Abhishek <abhishek.sharmaduo@gmail.com>
2022-10-07 18:56:08 +0530

    added 3 new model code

--- model.py
+++ model.py
@@ -1,3 +1,12 @@
 
 
 print("Adding all ml releated code here")
+
+def model1():
+    return "adding code for ML MOdel1"
+
+def model2():
+    return "adding code for ML MOdel2"
+
+def model3():
+    return "adding code for ML MOdel3"
